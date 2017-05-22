c87d7727b3314b07d12a3bd38f904e67603e2862
Tammo <tammorukat@gmail.com>
2017-05-22 18:05:51 +0100

    add c file

--- setup.py
+++ setup.py
@@ -8,9 +8,7 @@ from Cython.Distutils import build_ext
 ext_modules = [
     Extension(
         "cython_functions",
-        ["cython_functions.pyx"],
-        extra_compile_args = ["-Ofast", "-ffast-math", "-march=native", "-fopenmp" ],
-        extra_link_args=['-fopenmp'],
+        ["cython_functions.c"]
     )
 ]
 
@@ -22,8 +20,7 @@ setup(
     url='https://github.com/TammoR/OrMachine',
     py_modules=['ormachine','cython_functions'],
     ext_modules=cythonize(ext_modules),
-    setup_requires=["Cython >= 0.20"],
-    install_requires=['numpy','scipy','Cython']
+    install_requires=['numpy','scipy']
 )
 
 
