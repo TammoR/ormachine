d8aa841d02a3d605c4b2e4f6b332aa38c2bb1506
Tammo <tammorukat@gmail.com>
2017-05-23 00:14:57 +0100

    make setup mac compatible

--- setup.py
+++ setup.py
@@ -4,10 +4,12 @@ from distutils.core import setup
 from Cython.Build import cythonize
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import numpy
 
 ext_modules = [
     Extension(
         "cython_functions", ["cython_functions.c"],
+	include_dirs=[numpy.get_include()]
     )
 ]
 
