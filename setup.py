912d66530a980d5e9aa5cffb3524279cb6f9945a
Tammo <tammorukat@gmail.com>
2017-05-23 00:01:24 +0100

    remove cython dep.

--- setup.py
+++ setup.py
@@ -7,10 +7,7 @@ from Cython.Distutils import build_ext
 
 ext_modules = [
     Extension(
-        "cython_functions",
-        ["cython_functions.pyx"],
-        extra_compile_args = ["-Ofast", "-ffast-math", "-march=native", "-fopenmp" ],
-        extra_link_args=['-fopenmp'],
+        "cython_functions", ["cython_functions.c"],
     )
 ]
 
@@ -20,10 +17,8 @@ setup(
     author='Tammo Rukat',
     author_email='tammorukat@gmail.com',
     url='https://github.com/TammoR/OrMachine',
-    py_modules=['ormachine','cython_functions'],
-    ext_modules=cythonize(ext_modules),
-    setup_requires=["Cython >= 0.20"],
-    install_requires=['numpy','scipy']
+    py_modules=['ormachine'],
+    ext_modules=cythonize(ext_modules)
 )
 
 
