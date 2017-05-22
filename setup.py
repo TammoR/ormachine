a87a3c2cefdd4671f2ea0e6b807e974b99148253
Tammo <tammorukat@gmail.com>
2017-05-23 00:21:34 +0100

    remove cythonize module import

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
-from setuptools import setup, Extension
-from Cython.Build import cythonize
-from distutils.core import setup
 #from Cython.Build import cythonize
+from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import numpy
