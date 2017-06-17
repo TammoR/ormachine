4a842fe2a2b131f0e3150d5cc30c0bf5b016648e
Tammo <tammorukat@gmail.com>
2017-06-17 17:53:24 +0100

    add numpy to setup

--- setup_cythonize_singlecore.py
+++ setup_cythonize_singlecore.py
@@ -4,6 +4,7 @@ from distutils.core import setup
 from Cython.Build import cythonize
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import numpy
 
 ext_modules = [
     Extension(
